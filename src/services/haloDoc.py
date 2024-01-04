e902bd0e4d942e1985bb85e008afe6b0f682de46
ryosyntaxx <riodwi12174@gmail.com>
2024-01-04 10:18:16 +0700

    prepare completed

---
+++ src/services/haloDoc.py
@@ -0,0 +1,14 @@
+from APIRetrys import ApiRetry
+
+
+class HaloDocs:
+    def __init__(self) -> None:
+        pass
+
+
+    def extract_data(self):
+        pass
+
+    def main(self):
+        pass
+
